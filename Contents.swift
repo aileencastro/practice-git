40e18fb7f166325254bc319d0ddfaa5884933d81
abby <abbyramirez19@outlook.com>
2019-07-08 13:54:08 -0500

    First Commit

---
+++ Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+// use comments to help you keep track of ur code <3
+
+
+
+//Data Types involved: Strings and Variables and Inter
+
+//var name = "Aileen"
+//var name1 = "Dianna"
+//
+//var age = 17
+//
+//print("Hi \(name) and \(name1 )! I am \(age)")
+
+
+
+//Intention: working with Integers and Doubles
+//Data Types: Ints and Doubles
+
+//var int = 4
+//var double = 5.0
+//
+//print (Double(int) * double)
+
+var a = 12.4
+var b = 65.0
+var c = 31.0
+var d = 98
+
+print ( ((a+b+c)+Double(d))/4)
+
